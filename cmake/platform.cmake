fcf19b3e14c71e03a4ebc58bc55d1755807ff6d4
Teemu Ikonen <teemu.ikonen@king.com>
2017-06-12 19:53:46 +0300

    Policy update

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,5 +1,9 @@
 cmake_minimum_required(VERSION 3.0.0)
 
+# always link with absolute path, even implicit directories. avoids problems with
+# running targets on IDE when dll's are not in the environment search path.
+cmake_policy(SET CMP0060 NEW)
+
 if(UNIX AND ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(OSX 1)
 endif()
